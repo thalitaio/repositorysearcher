28ccbe574276f55a2dac94387cf4160bb8e85e4c
thalitaio <contatothalitanunes@gmail.com>
2021-08-12 18:37:31 -0300

    uploading files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <App title="Hello" user="Thalita" />,
+  document.getElementById('root')
+);
+
---
+++ src/pages/Home/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import * as S from './styled';
+import { useHistory } from 'react-router-dom';
+
+function App(props) {
+  const history = useHistory();
+  const [ usuario, setUsuario ] = useState('');
+  const [ erro, setErro ] = useState(false);
+
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`) 
+      .then(response => {
+        const repositories = response.data;
+        const repositoriesName = [];
+        repositories.map((repository) => {
+          repositoriesName.push(repository.name);
+        });
+        localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+        setErro(false);
+        history.push('/repositories');
+      })
+      .catch(err => {
+        setErro(true);
+      })
+  }
+
+  return (
+    <S.HomeContainer>
+      <S.Content> 
+        <S.Input className="usuarioInput"  placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+        </S.Content>
+        <S.Content>
+        { erro ? <S.ErrorMessage>Ocorreu um erro. Tente novamente.</S.ErrorMessage> : '' }
+        </S.Content>
+      
+    </S.HomeContainer>
+  );
+}
+
+export default App;
